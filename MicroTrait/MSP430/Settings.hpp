3ead0f4f48a26727e3f95e45820a3b38e1abcd8d
Steffen Fuchs <fuchs@kobold.com>
2020-10-06 15:00:38 +0200

    removed micro trait lib in order to re add as subtree

--- MicroTrait/MSP430/Settings.hpp
+++
@@ -1,9 +0,0 @@
-
-#ifndef MICROTRAIT_MSP430_SETTINGS_HPP_
-#define MICROTRAIT_MSP430_SETTINGS_HPP_
-
-
-//#define MSP430_DRIVERLIB_COMPATIBILITY /* if commented in the lib will be work internally like the driverlib / if commented out the lib will remove needless code  */
-
-
-#endif /* MICROTRAIT_MSP430_SETTINGS_HPP_ */
