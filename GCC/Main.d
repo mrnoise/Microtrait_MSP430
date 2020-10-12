052459849183ba6ed98705505d91fbf262643f59
MrNoise <steffenfuchs@me.com>
2020-10-12 08:50:37 +0200

    added lto

---
+++ GCC/Main.d
@@ -0,0 +1,39 @@
+Main.o: ../Main.cpp ../MicroTrait/MSP430/Settings.hpp \
+ ../MicroTrait/Universal/Register.hpp \
+ C:/Users/steff/iCloudDrive/Trunk/CCS/Microtrait_Test/MicroTrait/Misc/Details.hpp \
+ C:/Users/steff/iCloudDrive/Trunk/CCS/Microtrait_Test/MicroTrait/Misc/EnumBits.hpp \
+ ../MicroTrait/MSP430/GPIO/PortRegister.hpp \
+ C:/Users/steff/iCloudDrive/Trunk/CCS/Microtrait_Test/MicroTrait/Universal/Register.hpp \
+ C:/Users/steff/iCloudDrive/Trunk/CCS/Microtrait_Test/MicroTrait/MSP430/Settings.hpp \
+ C:/ti/ccs1011/ccs/ccs_base/msp430/include_gcc/msp430.h \
+ C:/ti/ccs1011/ccs/ccs_base/msp430/include_gcc/msp430fr2433.h \
+ C:/ti/ccs1011/ccs/ccs_base/msp430/include_gcc/iomacros.h \
+ C:/ti/ccs1011/ccs/ccs_base/msp430/include_gcc/in430.h \
+ ../MicroTrait/MSP430/GPIO/PortInterrupt.hpp \
+ C:/Users/steff/iCloudDrive/Trunk/CCS/Microtrait_Test/MicroTrait/Universal/Interrupt.hpp
+
+../MicroTrait/MSP430/Settings.hpp:
+
+../MicroTrait/Universal/Register.hpp:
+
+C:/Users/steff/iCloudDrive/Trunk/CCS/Microtrait_Test/MicroTrait/Misc/Details.hpp:
+
+C:/Users/steff/iCloudDrive/Trunk/CCS/Microtrait_Test/MicroTrait/Misc/EnumBits.hpp:
+
+../MicroTrait/MSP430/GPIO/PortRegister.hpp:
+
+C:/Users/steff/iCloudDrive/Trunk/CCS/Microtrait_Test/MicroTrait/Universal/Register.hpp:
+
+C:/Users/steff/iCloudDrive/Trunk/CCS/Microtrait_Test/MicroTrait/MSP430/Settings.hpp:
+
+C:/ti/ccs1011/ccs/ccs_base/msp430/include_gcc/msp430.h:
+
+C:/ti/ccs1011/ccs/ccs_base/msp430/include_gcc/msp430fr2433.h:
+
+C:/ti/ccs1011/ccs/ccs_base/msp430/include_gcc/iomacros.h:
+
+C:/ti/ccs1011/ccs/ccs_base/msp430/include_gcc/in430.h:
+
+../MicroTrait/MSP430/GPIO/PortInterrupt.hpp:
+
+C:/Users/steff/iCloudDrive/Trunk/CCS/Microtrait_Test/MicroTrait/Universal/Interrupt.hpp:
---
+++ GCC/subdir_vars.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../Main.cpp 
+
+LD_SRCS += \
+../msp430fr2433.ld 
+
+OBJS += \
+./Main.o 
+
+CPP_DEPS += \
+./Main.d 
+
+OBJS__QUOTED += \
+"Main.o" 
+
+CPP_DEPS__QUOTED += \
+"Main.d" 
+
+CPP_SRCS__QUOTED += \
+"../Main.cpp" 
+
+
