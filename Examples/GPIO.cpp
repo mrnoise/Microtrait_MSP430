e4de224924af1250f0f684282189f5cb121f2ed0
MrNoise <steffenfuchs@me.com>
2020-10-12 10:40:13 +0200

    moved gpio example to cpp/hpp

---
+++ Examples/GPIO.cpp
@@ -0,0 +1,61 @@
+#include "GPIO.hpp"
+#include "MicroTrait/MT.hpp"
+
+using namespace MT::MSP430;
+
+#ifdef MT_MSP430_USE_GPIO_COMPILE_TIME_CALLBACKS
+constexpr auto isr = GPIO::Interrupt::makeInterrupt(
+    GPIO::Interrupt::makeHandler(
+        GPIO::Interrupt::PORTS::PORT2,
+        []() {
+            GPIO::Port1 p1{};
+            GPIO::Port2 p2{};
+            p2.clearInterrupt(PIN::P3);
+            p1.toggleOutputOnPin(PIN::P0);
+        }));
+#endif
+
+void runGpioExample() {
+
+    MT::Universal::Register<&WDTCTL>  wdt{};
+    MT::Universal::Register<&PM5CTL0> pm5ctl{};
+    wdt.override(WDTPW | WDTHOLD);
+    pm5ctl.clear(LOCKLPM5);
+
+
+#ifndef MT_MSP430_USE_GPIO_COMPILE_TIME_CALLBACKS
+    GPIO::Interrupt::registerCallback(GPIO::Interrupt::PORTS::PORT2, []() {
+        GPIO::Port1 p1{};
+        GPIO::Port2 p2{};
+        p2.clearInterrupt(PIN::P3);
+        p1.toggleOutputOnPin(PIN::P0);
+    });
+#endif
+
+    GPIO::Port1 p1{};
+    GPIO::Port2 p2{};
+    p1.setOutputLowOnPin(PIN::P0);
+    p1.setAsOutputPin(PIN::P0);
+
+    p2.setAsInputPinWithPullUp(PIN::P3);
+    p2.selectInterruptEdge(INT_EDGE::HIGH_TO_LOW, PIN::P3);
+    p2.enableInterrupt(PIN::P3);
+    p2.clearInterrupt(PIN::P3);
+
+    __bis_SR_register(GIE);
+}
+
+
+#ifdef MT_MSP430_USE_GPIO_COMPILE_TIME_CALLBACKS
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector = PORT2_VECTOR
+__interrupt void Port_2(void)
+#elif defined(__GNUC__)
+void __attribute__((interrupt(PORT2_VECTOR))) Port_2(void)
+#else
+#error Compiler not supported!
+#endif
+{
+    std::get<isr.get_index(GPIO::Interrupt::PORTS::PORT2)>(isr.m_vectors)();
+}
+#endif
---
+++ Examples/GPIO.hpp
@@ -0,0 +1,7 @@
+
+#ifndef EXAMPLES_GPIO_HPP_
+#define EXAMPLES_GPIO_HPP_
+
+void runGpioExample();
+
+#endif /* EXAMPLES_GPIO_HPP_ */
