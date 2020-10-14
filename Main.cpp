bab4d237bba73e1a699fba4d9ab4041fd74cf28e
MrNoise <steffenfuchs@me.com>
2020-10-14 15:05:12 +0200

    added description for example

--- Main.cpp
+++ Main.cpp
@@ -19,7 +19,7 @@ int main(void) {
     //Toggels P1.0 by each press of P2.3 in Interrupt
     //runGpioExample();
 
-
+    //Toggels P1.0 in wdt intervall timer interrupt
     runWdtExample();
 
     while (1) {
