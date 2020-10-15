0393d77a82ef726f0a4c8aa18d1f671e49ae59a0
MrNoise <steffenfuchs@me.com>
2020-10-15 13:04:29 +0200

    extended GPIO example

--- Examples/GPIO.cpp
+++ Examples/GPIO.cpp
@@ -12,16 +12,37 @@ void runGpioExample() {
     P1OUT &= ~BIT0;// Clear P1.0 output latch for a defined power-on state
     P1DIR |= BIT0; // Set P1.0 to output direction
 
-    P2OUT |= BIT3; // Configure P1.3 as pulled-up
-    P2REN |= BIT3; // P1.3 pull-up register enable
-    P2IES |= BIT3; // P1.3 Hi/Low edge
-    P2IE |= BIT3;  // P1.3 interrupt enabled
-    P2IFG &= ~BIT3;// P1.3 IFG cleared
+    P2OUT |= BIT3; // Configure P2.3 as pulled-up
+    P2REN |= BIT3; // P2.3 pull-up register enable
+    P2IES |= BIT3; // P2.3 Hi/Low edge
+    P2IE |= BIT3;  // P2.3 interrupt enabled
+    P2IFG &= ~BIT3;// P2.3 IFG cleared
+
+    P1OUT |= BIT4; // Configure P1.4 as pulled-up
+    P1REN |= BIT4; // P1.4 pull-up register enable
+    P1IES |= BIT4; // P1.4 Hi/Low edge
+    P1IE |= BIT4;  // P1.4 interrupt enabled
+    P1IFG &= ~BIT4;// P1.4 IFG cleared
 
     __bis_SR_register(GIE);
 }
 
+
 // Port 1 interrupt service routine
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector = PORT1_VECTOR
+__interrupt void Port_1(void)
+#elif defined(__GNUC__)
+void __attribute__((interrupt(PORT1_VECTOR))) Port_1(void)
+#else
+#error Compiler not supported!
+#endif
+{
+    P1IFG &= ~BIT4;// Clear P1.3 IFG
+    P1OUT ^= BIT0;
+}
+
+// Port 2 interrupt service routine
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
 #pragma vector = PORT2_VECTOR
 __interrupt void Port_2(void)
