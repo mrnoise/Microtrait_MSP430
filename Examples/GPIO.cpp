ad46f7533c19c768999b79174e9705e1ba7f8074
MrNoise <steffenfuchs@me.com>
2020-10-28 14:01:09 +0100

    updated project and examples to MSP430FR6989

--- Examples/GPIO.cpp
+++ Examples/GPIO.cpp
@@ -12,17 +12,17 @@ void runGpioExample() {
     P1OUT &= ~BIT0;// Clear P1.0 output latch for a defined power-on state
     P1DIR |= BIT0; // Set P1.0 to output direction
 
-    P2OUT |= BIT3; // Configure P2.3 as pulled-up
-    P2REN |= BIT3; // P2.3 pull-up register enable
-    P2IES |= BIT3; // P2.3 Hi/Low edge
-    P2IE |= BIT3;  // P2.3 interrupt enabled
-    P2IFG &= ~BIT3;// P2.3 IFG cleared
+    P2OUT |= BIT4; // Configure P2.4 as pulled-up
+    P2REN |= BIT4; // P2.4 pull-up register enable
+    P2IES |= BIT4; // P2.4 Hi/Low edge
+    P2IE |= BIT4;  // P2.4 interrupt enabled
+    P2IFG &= ~BIT4;// P2.4 IFG cleared
 
-    P1OUT |= BIT4; // Configure P1.4 as pulled-up
-    P1REN |= BIT4; // P1.4 pull-up register enable
-    P1IES |= BIT4; // P1.4 Hi/Low edge
-    P1IE |= BIT4;  // P1.4 interrupt enabled
-    P1IFG &= ~BIT4;// P1.4 IFG cleared
+    P1OUT |= BIT1; // Configure P1.4 as pulled-up
+    P1REN |= BIT1; // P1.1 pull-up register enable
+    P1IES |= BIT1; // P1.1 Hi/Low edge
+    P1IE |= BIT1;  // P1.1 interrupt enabled
+    P1IFG &= ~BIT1;// P1.1 IFG cleared
 
     __bis_SR_register(GIE);
 }
@@ -38,7 +38,7 @@ void __attribute__((interrupt(PORT1_VECTOR))) Port_1(void)
 #error Compiler not supported!
 #endif
 {
-    P1IFG &= ~BIT4;// Clear P1.3 IFG
+    P1IFG &= ~BIT1;// Clear P1.1 IFG
     P1OUT ^= BIT0;
 }
 
@@ -52,6 +52,6 @@ void __attribute__((interrupt(PORT2_VECTOR))) Port_2(void)
 #error Compiler not supported!
 #endif
 {
-    P2IFG &= ~BIT3;// Clear P1.3 IFG
+    P2IFG &= ~BIT4;// Clear P2.4 IFG
     P1OUT ^= BIT0;
 }
