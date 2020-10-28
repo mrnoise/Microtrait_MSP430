4619c934ea955f226c536b378270cc77d2474707
MrNoise <steffenfuchs@me.com>
2020-10-28 11:04:38 +0100

    added TimerA example

---
+++ Examples/TIMERA.cpp
@@ -0,0 +1,82 @@
+#include "GPIO.hpp"
+#include "driverlib/MSP430FR2xx_4xx/driverlib.h"
+
+#define GPIO_PORT_LED1 GPIO_PORT_P1
+#define GPIO_PIN_LED1  GPIO_PIN0
+
+#define COMPARE_VALUE 50000
+
+void runTimerAExample() {
+
+    //Stop Watchdog Timer
+    WDT_A_hold(WDT_A_BASE);
+
+    //Set LED1 as an output pin.
+    GPIO_setAsOutputPin(
+        GPIO_PORT_LED1,
+        GPIO_PIN_LED1);
+
+    /*
+	 * Disable the GPIO power-on default high-impedance mode to activate
+	 * previously configured port settings
+	 */
+    PMM_unlockLPM5();
+
+    //Start timer in continuous mode sourced by SMCLK
+    Timer_A_initContinuousModeParam initContParam = {};
+    initContParam.clockSource                     = TIMER_A_CLOCKSOURCE_SMCLK;
+    initContParam.clockSourceDivider              = TIMER_A_CLOCKSOURCE_DIVIDER_1;
+    initContParam.timerInterruptEnable_TAIE       = TIMER_A_TAIE_INTERRUPT_DISABLE;
+    initContParam.timerClear                      = TIMER_A_DO_CLEAR;
+    initContParam.startTimer                      = false;
+    Timer_A_initContinuousMode(TIMER_A1_BASE, &initContParam);
+
+    //Initiaze compare mode
+    Timer_A_clearCaptureCompareInterrupt(TIMER_A1_BASE,
+        TIMER_A_CAPTURECOMPARE_REGISTER_0);
+
+    Timer_A_initCompareModeParam initCompParam = {};
+    initCompParam.compareRegister              = TIMER_A_CAPTURECOMPARE_REGISTER_0;
+    initCompParam.compareInterruptEnable       = TIMER_A_CAPTURECOMPARE_INTERRUPT_ENABLE;
+    initCompParam.compareOutputMode            = TIMER_A_OUTPUTMODE_OUTBITVALUE;
+    initCompParam.compareValue                 = COMPARE_VALUE;
+    Timer_A_initCompareMode(TIMER_A1_BASE, &initCompParam);
+
+    Timer_A_startCounter(TIMER_A1_BASE,
+        TIMER_A_CONTINUOUS_MODE);
+
+    //Enter LPM0, enable interrupts
+    __bis_SR_register(LPM0_bits + GIE);
+
+    //For debugger
+    __no_operation();
+}
+
+
+//******************************************************************************
+//
+//This is the TIMER1_A0 interrupt vector service routine.
+//
+//******************************************************************************
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector = TIMER1_A0_VECTOR
+__interrupt
+#elif defined(__GNUC__)
+__attribute__((interrupt(TIMER1_A0_VECTOR)))
+#endif
+    void
+    TIMER1_A0_ISR(void) {
+    uint16_t compVal = Timer_A_getCaptureCompareCount(TIMER_A1_BASE,
+                           TIMER_A_CAPTURECOMPARE_REGISTER_0)
+                       + COMPARE_VALUE;
+
+    //Toggle LED1
+    GPIO_toggleOutputOnPin(
+        GPIO_PORT_LED1,
+        GPIO_PIN_LED1);
+
+    //Add Offset to CCR0
+    Timer_A_setCompareValue(TIMER_A1_BASE,
+        TIMER_A_CAPTURECOMPARE_REGISTER_0,
+        compVal);
+}
---
+++ Examples/TIMERA.hpp
@@ -0,0 +1,8 @@
+#ifndef EXAMPLES_TIMERA_HPP_
+#define EXAMPLES_TIMERA_HPP_
+
+
+void runTimerAExample();
+
+
+#endif /* EXAMPLES_TIMERA_HPP_ */
