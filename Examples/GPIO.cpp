b44b58dde53249a2b5dbce27420e9266ff4520d4
MrNoise <steffenfuchs@me.com>
2020-12-19 17:15:16 +0100

    changed GPIO example to use __even_in_range intrinsic fro ISR

--- Examples/GPIO.cpp
+++ Examples/GPIO.cpp
@@ -41,8 +41,19 @@ void __attribute__((interrupt(PORT1_VECTOR))) Port_1(void)
 #error Compiler not supported!
 #endif
 {
-    GPIO_clearInterrupt(GPIO_PORT_S1, GPIO_PIN_S1);
-    GPIO_toggleOutputOnPin(GPIO_PORT_LED1, GPIO_PIN_LED1);
+    switch (__even_in_range(P1IV, P1IV_P1IFG7)) {
+        case P1IV_NONE: break;
+        case P1IV_P1IFG0: break;
+        case P1IV_P1IFG1:
+            GPIO_toggleOutputOnPin(GPIO_PORT_LED1, GPIO_PIN_LED1);
+            break;
+        case P1IV_P1IFG2: break;
+        case P1IV_P1IFG3: break;
+        case P1IV_P1IFG4: break;
+        case P1IV_P1IFG5: break;
+        case P1IV_P1IFG6: break;
+        case P1IV_P1IFG7: break;
+    }
 }
 
 
@@ -56,6 +67,17 @@ void __attribute__((interrupt(PORT2_VECTOR))) Port_2(void)
 #error Compiler not supported!
 #endif
 {
-    GPIO_clearInterrupt(GPIO_PORT_P2, GPIO_PIN4);
-    GPIO_toggleOutputOnPin(GPIO_PORT_LED1, GPIO_PIN_LED1);
+    switch (__even_in_range(P2IV, P2IV_P2IFG7)) {
+        case P2IV_NONE: break;
+        case P2IV_P2IFG0: break;
+        case P2IV_P2IFG1: break;
+        case P2IV_P2IFG2: break;
+        case P2IV_P2IFG3: break;
+        case P2IV_P2IFG4:
+            GPIO_toggleOutputOnPin(GPIO_PORT_LED1, GPIO_PIN_LED1);
+            break;
+        case P2IV_P2IFG5: break;
+        case P2IV_P2IFG6: break;
+        case P2IV_P2IFG7: break;
+    }
 }
