711a7d196a741f46a1e7f66130976a8abbb30f63
MrNoise <steffenfuchs@me.com>
2021-01-27 14:05:58 +0100

    added init to EUSCIA example

--- Examples/EUSCIA.cpp
+++ Examples/EUSCIA.cpp
@@ -8,6 +8,22 @@ void runEusciAExample() {
     WdtA wdt{};
     wdt.hold();
 
+    EUSCIA::UART::initParam param{
+        // Baudrate 9600 at 1MHz SMCLCK
+        EUSCIA::UART::CLOCKSOURCE::SMCLK,
+        6,
+        8,
+        17,
+        EUSCIA::UART::PARITY::NO,
+        EUSCIA::UART::ENDIAN::MSB_FIRST,
+        EUSCIA::UART::STOPBIT::ONE,
+        EUSCIA::UART::MODE::UART,
+        EUSCIA::UART::BAUD_GENERATION::OVERSAMPLING
+    };
+
+    EUSCIA::UART::A0 a0;
+    a0.init(param);
+
 
     //Enter LPM0, enable interrupts
     __bis_SR_register(LPM0_bits + GIE);
